93f4468e2c7dfd97396192a46d49453a0b2d5213
cyberhunter00 <m.anasqureshi@gmail.com>
2017-04-13 09:59:39 -0700

    Update screenshot.ps1

--- screenshot.ps1
+++ screenshot.ps1
@@ -1,53 +1,65 @@
-function Get-ScreenShot
-{
-    [CmdletBinding()]
-    param(
-        [parameter(Position  = 0, Mandatory = 0, ValueFromPipelinebyPropertyName = 1)]
-        [ValidateNotNullOrEmpty()]
-        [string]$OutPath = "$env:USERPROFILE\Documents\ScreenShot",
- 
-        #screenshot_[yyyyMMdd_HHmmss_ffff].png
-        [parameter(Position  = 1, Mandatory = 0, ValueFromPipelinebyPropertyName = 1)]
-        [ValidateNotNullOrEmpty()]
-        [string]$FileNamePattern = 'screenshot_{0}.png',
- 
-        [parameter(Position  = 2,Mandatory = 0, ValueFromPipeline = 1, ValueFromPipelinebyPropertyName = 1)]
-        [ValidateNotNullOrEmpty()]
-        [int]$RepeatTimes = 0,
- 
-        [parameter(Position  = 3, Mandatory = 0, ValueFromPipelinebyPropertyName = 1)]
-        [ValidateNotNullOrEmpty()]
-        [int]$DurationMs = 1
-     )
- 
-     begin
-     {
-        $ErrorActionPreference = 'Stop'
-        Add-Type -AssemblyName System.Windows.Forms
- 
-        if (-not (Test-Path $OutPath))
-        {
-            New-Item $OutPath -ItemType Directory -Force
-        }
-     }
- 
-     process
-     {
-        0..$RepeatTimes `
-        | %{
-            $fileName = $FileNamePattern -f (Get-Date).ToString('yyyyMMdd_HHmmss_ffff')
-            $path = Join-Path $OutPath $fileName
- 
-            $b = New-Object System.Drawing.Bitmap([System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width, [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height)
-            $g = [System.Drawing.Graphics]::FromImage($b)
-            $g.CopyFromScreen((New-Object System.Drawing.Point(0,0)), (New-Object System.Drawing.Point(0,0)), $b.Size)
-            $g.Dispose()
-            $b.Save($path)
- 
-            if ($RepeatTimes -ne 0)
-            {
-                Start-Sleep -Milliseconds $DurationMs
-            }
-        }
-    }
-}
+[cmdletbinding()]            
+param(            
+  [string]$Width,            
+  [string]$Height,            
+  [String]$FileName = "Screenshot"            
+
+)            
+
+#Function to take screenshot. This function takes the width and height of the screen that has            
+#to be captured            
+
+function Take-Screenshot{            
+[cmdletbinding()]            
+param(            
+ [Drawing.Rectangle]$bounds,             
+ [string]$path            
+)             
+   $bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height            
+   $graphics = [Drawing.Graphics]::FromImage($bmp)            
+   $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)            
+   $bmp.Save($path)            
+   $graphics.Dispose()            
+   $bmp.Dispose()            
+}            
+
+#Function to get the primary monitor resolution.            
+#This code is sourced from             
+# http://techibee.com/powershell/powershell-script-to-get-desktop-screen-resolution/1615            
+
+function Get-ScreenResolution {            
+ $Screens = [system.windows.forms.screen]::AllScreens                        
+ foreach ($Screen in $Screens) {            
+  $DeviceName = $Screen.DeviceName            
+  $Width  = $Screen.Bounds.Width            
+  $Height  = $Screen.Bounds.Height            
+  $IsPrimary = $Screen.Primary                        
+  $OutputObj = New-Object -TypeName PSobject            
+  $OutputObj | Add-Member -MemberType NoteProperty -Name DeviceName -Value $DeviceName            
+  $OutputObj | Add-Member -MemberType NoteProperty -Name Width -Value $Width            
+  $OutputObj | Add-Member -MemberType NoteProperty -Name Height -Value $Height            
+  $OutputObj | Add-Member -MemberType NoteProperty -Name IsPrimaryMonitor -Value $IsPrimary            
+  $OutputObj                        
+ }            
+}            
+
+#Main script begins            
+
+#By default captured screenshot will be saved in %temp% folder            
+#You can override it here if you want            
+$Filepath = join-path $env:temp $FileName            
+
+[void] [Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")            
+[void] [Reflection.Assembly]::LoadWithPartialName("System.Drawing")            
+
+if(!($width -and $height)) {            
+
+ $screen = Get-ScreenResolution | ? {$_.IsPrimaryMonitor -eq $true}            
+ $Width = $screen.Width            
+ $Height = $screen.height            
+}            
+
+$bounds = [Drawing.Rectangle]::FromLTRB(0, 0, $Screen.Width, $Screen.Height)            
+
+Take-Screenshot -Bounds $bounds -Path "$Filepath.png"            
+#Now you have the screenshot
